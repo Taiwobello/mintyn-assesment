88399735f7ad31bd6ae7d453eee35ba66c3bd271
Taiwobello <bellotaiwolateef1999@gmail.com>
2022-10-29 11:23:43 +0100

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">Mintyn Test</div>;
+}
+
+export default App;
---
+++ utils/constants.js
@@ -0,0 +1,53 @@
+export const sideBarLinks = [
+  {
+    title: "Main",
+    children: [
+      {
+        title: "Overview",
+        icon_src: "overview.svg",
+      },
+    ],
+  },
+  {
+    title: "Payments",
+    children: [
+      {
+        title: "All Payments",
+        icon_src: "all-payment.svg",
+      },
+      {
+        title: "Reconcilled Payments",
+        icon_src: "reconcile-payment.svg",
+      },
+      {
+        title: "Un - Reconcilled Payments",
+        icon_src: "un-reconcile-payment.svg",
+      },
+      {
+        title: "Manual Settlement",
+        icon_src: "manual-settlement.svg",
+      },
+    ],
+  },
+  {
+    title: "All Orders",
+    children: [
+      {
+        title: "All Orders",
+        icon_src: "order.svg",
+      },
+      {
+        title: "Pending Orders",
+        icon_src: "pending-order.svg",
+      },
+      {
+        title: "Reconcilled Orders",
+        icon_src: "reconciled-order.svg",
+      },
+    ],
+  },
+  {
+    title: "Merchant Profile",
+    icon_src: "single.svg",
+  },
+];
